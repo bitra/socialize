8e2b8fa5f827a004d7e622f4167825a7c29197ac
Elliot Haughin <elliot@haughin.com>
2010-08-16 16:49:52 +0100

    Twitter config placeholders

--- application/third_party/elliothaughin/socialize/networks/twitter/config/twitter.php
+++ application/third_party/elliothaughin/socialize/networks/twitter/config/twitter.php
@@ -1,4 +1,6 @@
 <?php
 	
 	// Currently just a placeholder
-	$config['twitter_something'] 		= '';
+	$config['twitter_api_key'] 		= '';
+	$config['twitter_oauth_consumer_key'] = '';
+	$config['twitter_oauth_consumer_secret'] = '';
