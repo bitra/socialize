76ae9621708bcf79d8acec018cc78545bd6ec158
Elliot Haughin <elliot@haughin.com>
2010-08-16 22:06:55 +0100

    Tidy config

--- application/third_party/elliothaughin/socialize/networks/twitter/config/twitter.php
+++ application/third_party/elliothaughin/socialize/networks/twitter/config/twitter.php
@@ -1,6 +1,6 @@
 <?php
 	
 	// Currently just a placeholder
-	$config['twitter_api_key'] 		= '';
-	$config['twitter_oauth_consumer_key'] = '';
-	$config['twitter_oauth_consumer_secret'] = '';
+	$config['twitter_api_key'] 					= '';
+	$config['twitter_oauth_consumer_key'] 		= '';
+	$config['twitter_oauth_consumer_secret']	= '';
