b691edb6d4f2b01c142c5f9aad2fe3c53ffcf0a6
Elliot Haughin <elliot@haughin.com>
2010-08-16 13:17:56 +0100

    Blanking facebook api keys (now need to add your own)

--- application/third_party/elliothaughin/socialize/config/networks/facebook.php
+++ application/third_party/elliothaughin/socialize/config/networks/facebook.php
@@ -1,7 +1,7 @@
 <?php
 
-	$config['facebook_app_id'] 		= '122739461073264';
-	$config['facebook_api_key'] 	= '645fe653f30af18265b3d32bbcb1b7da';
-	$config['facebook_api_secret'] 	= 'd036e33267ca182d499b8395d254ad3f';
-	$config['facebook_site_name'] 	= 'Haughin Playground';
+	$config['facebook_app_id'] 		= '';
+	$config['facebook_api_key'] 	= '';
+	$config['facebook_api_secret'] 	= '';
+	$config['facebook_site_name'] 	= '';
 	$config['facebook_admins']		= ''; // This is your own facebook user id.
